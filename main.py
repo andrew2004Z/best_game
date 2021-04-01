36628e1afb413fefb45313864119b68358a0ec47
andrew2004Z <60035892+andrew2004Z@users.noreply.github.com>
2021-04-01 11:21:25 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import sys
+import sdl2
+import sdl2.ext
+
+
+class Window:
+    def __init__(self, size, color, name):
+        self.size = size
+        self.color = color
+        self.name = name
+
+    def run(self):
+        sdl2.ext.init()
+        window = sdl2.ext.Window(self.name, size=self.size)
+        window.show()
+        running = True
+        while running:
+            events = sdl2.ext.get_events()
+            for event in events:
+                if event.type == sdl2.SDL_QUIT:
+                    running = False
+                    break
+            window.refresh()
+        return 0
+
+
+def run():
+    sdl2.ext.init()
+    window = sdl2.ext.Window("Best", size=(1080, 720))
+    window.show()
+    running = True
+    while running:
+        events = sdl2.ext.get_events()
+        for event in events:
+            if event.type == sdl2.SDL_QUIT:
+                running = False
+                break
+        window.refresh()
+    return 0
+
+#window = Window((1080, 720), (240, 40, 40), "Best Game")
+if __name__ == "__main__":
+    sys.exit(run())
