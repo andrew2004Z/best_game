e9ad05d028f290db8770bc814fa37cb5a52a4004
andrew2004Z <60035892+andrew2004Z@users.noreply.github.com>
2021-04-07 12:42:52 +0300

    Add files via upload

--- draw.py
+++ draw.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 
 def draw_menu(path):
     image = Image.open(path)
@@ -24,4 +24,17 @@ def draw_rec_f(w_f, h_f, x, y, w, h, char):
 #f.write(str(draw_rec_f(1080, 720, 362, 283, 386, 90, '00416A')))
 #f.close()
 
-print(draw_menu('menu.png'))
+#rint(draw_menu('menu.png'))
+def draw_1(path, color, new_image):
+    image = Image.open(path)
+    size = image.size
+    pix = image.load()
+    draw = ImageDraw.Draw(new_image)
+    for i in range(size[0]):
+        for j in range(size[1]):
+            if pix[i, j] == color:
+                #pix[i, j] = (255, 255, 255)
+                draw.point((i, j), (255, 255, 255))
+new_image = Image.open('1.png')
+draw_1('1.png', (255, 147, 171), new_image)
+new_image.save('2.png', 'PNG')
