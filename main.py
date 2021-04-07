fa59dcb53b778c37862592a6ee7dbf74fe4d0dd6
andrew2004Z <60035892+andrew2004Z@users.noreply.github.com>
2021-04-07 12:30:23 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import sdl2
 import sdl2.ext
 import sdl2.sdlgfx
+from math import sin, cos, sqrt
 from PIL import Image
 
 
@@ -48,6 +49,130 @@ class Window:
         processor = sdl2.ext.TestEventProcessor()
         processor.run(self.window)
 
+    def line_vert(self, x, y, l, color=(0, 0, 0)):
+        # new_image = Image.new("RGB", (500, 500), (255, 255, 255))
+        Window.d1_point(self, x, y, self.window.get_surface(), color)
+        if l < 0:
+            for i in range(l * -1):
+                Window.d1_point(self, x - 1, y - 1, self.window.get_surface(), color)
+                y = y - 1
+        else:
+            for i in range(l):
+                # print(x, y)
+                Window.d1_point(self, x, y + 1, self.window.get_surface(), color)
+                y = y + 1
+
+    def line_goriz(self, x, y, l, color=(0, 0, 0)):
+        # new_image = Image.new("RGB", (500, 500), (255, 255, 255))
+        Window.d1_point(self, x, y, self.window.get_surface(), color)
+        if l < 0:
+            for i in range(l * -1):
+                Window.d1_point(self, x-1, y, self.window.get_surface(), color)
+                x = x - 1
+        else:
+            for i in range(l):
+                # print(x, y)
+                Window.d1_point(self, x, y, self.window.get_surface(), color)
+                x = x + 1
+
+    def rectangle(self,  x, y, w, h, color=(0, 0, 0)):
+        # new_image = Image.new("RGB", (500, 500), (255, 255, 255))
+        # draw = ImageDraw.Draw(new_image)
+        xyw = [x + w, y]
+        xyh = [x, y + h]
+        Window.d1_point(self, x, y, self.window.get_surface(), color)
+        Window.line_goriz(self, x, y, w, color)
+        Window.line_vert(self, x, y, h, color)
+        Window.line_goriz(self, xyh[0], xyh[1], w, color)
+        Window.line_vert(self, xyw[0], xyw[1], h, color)
+
+
+    def rectangle1(self,  x, y, w, h, color=(0, 0, 0)):
+        Window.d1_point(self, x, y, self.window.get_surface(), color)
+        for i in range(x, x + w + 1):
+            for j in range(y, y + h + 1):
+                Window.d1_point(self, i, j, self.window.get_surface(), color)
+
+
+
+    def vert_dio(sp, new_image, color=(0, 0, 0), w=50, h=50):
+        w_v = w * len(sp) + 5 * len(sp)
+        max_sp = max(sp)
+        line_goriz(w + 20, max_sp * h + h, w_v * 3, new_image, color)
+        for j, i in enumerate(sp):
+            rectangle(w + 20, max_sp * h + h - (i * 10), 10, i * 10, new_image, color)
+            w = 50
+            w = w + ((j + 1) * 20)
+
+    def draw_line(x1, y1, x2, y2, new_image, color=(0, 0, 0)):
+        deltay = abs(y1 - y2)
+        deltax = abs(x1 - x2)
+        draw = ImageDraw.Draw(new_image)
+
+        if y1 < y2:
+            deltay = abs(y2 - y1)
+        if x1 < x2:
+            deltax = abs(x2 - x1)
+        lenght = max([deltax, deltay])
+        print(lenght)
+        print(deltax, deltay)
+        dx = deltax / lenght
+        dy = deltay / lenght
+        x = x1
+        y = y1
+        while lenght != 0:
+            draw.point((round(x), round(y)), color)
+            x += dx
+            y += dy
+            lenght -= 1
+
+    def draw_square(x1, y1, lenht, new_image, color=(0, 0, 0)):
+        rectangle(x1, y1, lenht, lenht, new_image, color)
+
+    def ROUND(a):
+        return int(a + 0.5)
+
+    def drawDDA(x1, y1, x2, y2, new_image, color=(0, 0, 0)):
+        draw = ImageDraw.Draw(new_image)
+        x, y = x1, y1
+        length = abs((x2 - x1) if abs(x2 - x1) > abs(y2 - y1) else (y2 - y1))
+        dx = (x2 - x1) / float(length)
+        dy = (y2 - y1) / float(length)
+        draw.point((ROUND(x), ROUND(y)), color)
+        for i in range(int(length)):
+            x += dx
+            y += dy
+            draw.point((ROUND(x), ROUND(y)), color)
+
+    def hexagon(x, y, length, new_image, color=(0, 0, 0)):
+        draw = ImageDraw.Draw(new_image)
+        bside = round(length * sin(60 / 180 * 3.14))
+        cside = round(length * sin(30 / 180 * 3.14))
+        drawDDA(x, y, x + length, y, new_image, color)
+        drawDDA(x, y - bside * 2, x + length, y - bside * 2, new_image, color)
+        y1, x1 = y - bside, x - cside
+        y2, x2 = y - bside, x + length + cside
+        drawDDA(x, y, x1, y1, new_image, color)
+        drawDDA(x1, y1, x, y - bside * 2, new_image, color)
+        drawDDA(x + length, y, x2, y2, new_image, color)
+        drawDDA(x + length, y - bside * 2, x2, y2, new_image, color)
+        draw.point((x2, y2), fill=color)
+
+    def pentagon(x, y, length, new_image, color=(0, 0, 0)):
+        draw = ImageDraw.Draw(new_image)
+        bside = round(length * sin(54 / 180 * 3.14)) * 2
+        cside = round(length * sin(36 / 180 * 3.14))
+        heigth1 = round(sqrt(pow(bside, 2) - pow(length / 2, 2)))
+        drawDDA(x, y, x + length, y, new_image, color)
+        y1, x1 = y - heigth1 + cside, x + length / 2 - bside / 2
+        y2, x2 = y1, x + length / 2 + bside / 2
+        drawDDA(x + length / 2, y - heigth1, x1, y1, new_image, color)
+        drawDDA(x + length / 2, y - heigth1, x2, y2, new_image, color)
+        drawDDA(x1, y1, x, y, new_image, color)
+        drawDDA(x2, y2, x + length, y, new_image, color)
+
+
+
     def run(self):
         sdl2.ext.init()
         # window = sdl2.ext.Window(self.name, size=self.size)
@@ -62,10 +187,54 @@ class Window:
                     running = False
                     break
                 elif event.type == sdl2.SDL_KEYDOWN:
-                    if event.key.keysym.sym == sdl2.SDLK_UP:
-                        for i in range(10, 100):
-                            for j in range(10, 20):
-                                Window.d1_point(self, i, j, self.window.get_surface(), (100, 100, 100))
+                    if event.key.keysym.sym == sdl2.SDLK_SPACE:
+                        x, y = 0, 700
+                        Window.rectangle1(self, 0, 700, 20, 20)
+                    elif event.key.keysym.sym == sdl2.SDLK_RIGHT:
+                        try:
+                            Window.rectangle1(self, x, y, 20, 20, color=(100, 10, 100))
+                            Window.rectangle1(self, x + 10, y, 20, 20)
+                            x = x + 10
+                        except:
+                            Window.rectangle1(self, x, y, 20, 20, color=(100, 10, 100))
+                            Window.rectangle1(self, x, y, 20, 20)
+                    elif event.key.keysym.sym == sdl2.SDLK_LEFT:
+                        try:
+                            Window.rectangle1(self, x, y, 20, 20, color=(100, 10, 100))
+                            Window.rectangle1(self, x - 10, y, 20, 20)
+                            x = x - 10
+                        except:
+                            Window.rectangle1(self, x, y, 20, 20, color=(100, 10, 100))
+                            Window.rectangle1(self, x, y, 20, 20)
+                    elif event.key.keysym.sym == sdl2.SDLK_UP:
+                        try:
+                            Window.rectangle1(self, x, y, 20, 20, color=(100, 10, 100))
+                            Window.rectangle1(self, x, y - 10, 20, 20)
+                            y = y - 10
+                        except:
+                            Window.rectangle1(self, x, y, 20, 20, color=(100, 10, 100))
+                            Window.rectangle1(self, x, y, 20, 20)
+                    elif event.key.keysym.sym == sdl2.SDLK_DOWN:
+                        try:
+                            Window.rectangle1(self, x, y, 20, 20, color=(100, 10, 100))
+                            Window.rectangle1(self, x, y + 10, 20, 20)
+                            y = y + 10
+                        except:
+                            Window.rectangle1(self, x, y, 20, 20, color=(100, 10, 100))
+                            Window.rectangle1(self, x, y, 20, 20)
+                    elif event.key.keysym.sym == sdl2.SDLK_r:
+                        Window.fill_Window(self, (100, 90, 7))
+                        x, y = 0, 700
+                        #try:
+                        #    Window.rectangle1(self, x, y, 20, 20, color=(100, 10, 100))
+                        #    Window.rectangle1(self, x, y + 10, 20, 20)
+                        #    y = y + 10
+                        #except:
+                        #    Window.rectangle1(self, x, y, 20, 20, color=(100, 10, 100))
+                        #    Window.rectangle1(self, x, y, 20, 20)
+                        #for i in range(10, 100):
+                        #    for j in range(10, 20):
+                        #        Window.d1_point(self, i, j, self.window.get_surface(), (100, 100, 100))
                         # Window.d1_point(self, 11, 20, self.window.get_surface(), (0, 0, 0))
                         # Window.d1_point(self, 12, 20, self.window.get_surface(), (0, 0, 0))
                         # Window.d1_point(self, 13, 20, self.window.get_surface(), (0, 0, 0))
@@ -87,7 +256,7 @@ class Window:
 
 
 def main():
-    window = Window((1080, 720), "Best Game")
+    window = Window((1081, 721), "Best Game")
 
     window.run()
     # window.fill_Window((240, 0, 0))
